6d3f424c4be665f5a8a0f30b4301b7cdb0458019
Depiction <nicalcoca@gmail.com>
2023-08-18 21:37:47 -0500

    Sincronizado

--- src/components/ConsultasGraficas.js
+++ src/components/ConsultasGraficas.js
@@ -2,32 +2,15 @@ import React, { useState, useRef, useEffect } from "react";
 import useEfectoTraerProyectos from "./EfectoTraerProyectos";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import style from "../static/style.css";
+import "../static/style.css";
 
 
 // Items con estilo que depende de la función handleClick
-const SelectableItem = ({ item, isSelected, onSelect, selectedItems, setSelectedItems }) => {
-
-    const itemUpdater = (id, setSelectedItems) => {
-
-        // Where id is the key of the element
-        setSelectedItems((prevSelectedItems) => {
-
-            if (prevSelectedItems.includes(id)) {
-                console.log('Y', id);
-                return prevSelectedItems.filter((pk) => pk !== id);
-            } else {
-                console.log('N', id);
-                return [...prevSelectedItems, id];
-            }
-            
-        });
-
-    }
+const SelectableItem = ({ item, isSelected, onSelect }) => {
 
     return (
         <div className="selectable"
-            onClick={() => { onSelect(item.pk); itemUpdater(item.pk, setSelectedItems) }}
+            onClick={() => { onSelect(item.pk); }}
             style={{
                 cursor: 'pointer',
                 backgroundColor: isSelected ? 'lightcoral' : 'white',
@@ -44,83 +27,56 @@ const SelectableItem = ({ item, isSelected, onSelect, selectedItems, setSelected
 
 
 // Lista de items
-const SelectableItemList = ({ items, data, setData, mapRef }) => {
+const SelectableItemList = ({ items, data, setData }) => {
 
     const [selectedItems, setSelectedItems] = useState([]);
-    console.log('EYYYY', selectedItems);
+    
+    const handleClick = (id) => {
 
-    // Agrega o elimina items del Hook selectedItems    
-    const handleClick = async (selectedItems) => {
+        setSelectedItems((selectedItems) => {
 
-        // Crear una forma contenedora de datos
-        const uploadData = new FormData();
-        
+            if (selectedItems.includes(id)) {
+                    return selectedItems.filter((pk) => pk !== id);
+            } else {
+                return [...selectedItems, id];
+            }
+            
+        });
+
+    };
+
+    const updater = async (sI) => {
+
         try {
-
+    
             const response = await fetch(
-
+    
                 "http://127.0.0.1:8000/visual/enviar_puntos_visuales",
                 {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({'id': selectedItems})
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({'id': sI})
                 }
-
+    
             );
-            
-            const data_f = await response.json();
-            const array = JSON.parse(data_f.data);
+
+            const data_d = await response.json();
+            const array = JSON.parse(data_d.data);
             setData(array);
-
+    
         } catch (error) {
-
+    
             console.error(error);
             
         }
 
-        if ( data === null ) {
+    }
 
-            console.log('null');
-            const map = mapRef.current
-
-        } else {
-
-            console.log('notnull');
-            // Add puntos to the map
-            data.forEach((punto) => {
-
-                var markerOptions = {
-                    color: "green",
-                    fillColor: "yellow",
-                    fillOpacity: 0.8,
-                    radius: 0.8
-                };
-
-                var marker;
-
-                // Extract latitude and longitude from punto.coordinates string
-                const regex = /POINT \(([-\d.]+) ([-\d.]+)\)/;
-                const matches = punto.fields.posicion.match(regex);
-
-                if (matches) {
-
-                    const longitude = parseFloat(matches[1]);
-                    const latitude = parseFloat(matches[2]);
-
-                    // Create marker with extracted latitude and longitude
-                    marker = L.circle([latitude, longitude], markerOptions);
-                    marker.addTo(mapRef.current);
-
-                }
-                
-            });
-
-        }
-
-
-    };
+    useEffect(() => {
+        updater(selectedItems);
+    }, [selectedItems])
 
     return (
 
@@ -135,8 +91,6 @@ const SelectableItemList = ({ items, data, setData, mapRef }) => {
                     item={item}
                     isSelected={selectedItems.includes(item.pk)}
                     onSelect={handleClick}
-                    selectedItems={selectedItems}
-                    setSelectedItems={setSelectedItems}
                 />
 
             ))}
@@ -152,20 +106,16 @@ const SelectableItemList = ({ items, data, setData, mapRef }) => {
 const Inicio = () => {
 
     const proyectos = useEfectoTraerProyectos("http://127.0.0.1:8000/visual/enviar_proyectos_visuales");
-    // var subProject = proyectos.filter((item) => selectedItems.includes(item.pk));
     const [data, setData] = useState(null);
+    const mapRef = useRef(null);
 
     // Mapa
     useEffect(() => {
-
-        // console.log('b', !mapRef.current);
 
         if (!mapRef.current) {
 
             // Create a Leaflet map
             var map = L.map("leaflet-map").setView([4.691901, -74.074223], 5);
-
-            // console.log('c', !mapRef.current);
 
             L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
                 maxZoom: 19,
@@ -178,7 +128,56 @@ const Inicio = () => {
 
     }, [])
 
-    const mapRef = useRef(null);
+
+    if ( data === null || Object.keys(data).length === 0) {
+
+        try {
+
+            mapRef.current.eachLayer(function(layer) {
+
+                if (!layer._url) {
+                    mapRef.current.removeLayer(layer);
+                }
+    
+            });
+
+        } catch {
+            //pass
+        }
+
+
+    } else {
+
+        // Add puntos to the map
+        data.forEach((punto) => {
+
+            var markerOptions = {
+                color: "green",
+                fillColor: "yellow",
+                fillOpacity: 0.8,
+                radius: 0.8
+            };
+
+            var marker;
+
+            // Extract latitude and longitude from punto.coordinates string
+            const regex = /POINT \(([-\d.]+) ([-\d.]+)\)/;
+            const matches = punto.fields.posicion.match(regex);
+
+            if (matches) {
+
+                const longitude = parseFloat(matches[1]);
+                const latitude = parseFloat(matches[2]);
+
+                // Create marker with extracted latitude and longitude
+                marker = L.circle([latitude, longitude], markerOptions);
+                marker.addTo(mapRef.current);
+
+            }
+            
+        });
+
+    }
 
     // Renderar puntos
     return (
@@ -195,12 +194,11 @@ const Inicio = () => {
                         items={proyectos}
                         data={data}
                         setData={setData}
-                        mapRef={mapRef}
                     />
 
                 </div>
 
-                <div id="leaflet-map" className="leaflet-map right-panel"></div>
+                <div id="leaflet-map" className="leaflet-map"></div>
 
             </div>
             
